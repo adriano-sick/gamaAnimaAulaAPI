3f82a010545c8243761020f7ebf41f50135e4024
adriano-sick <sick.zero.boy@gmail.com>
2021-10-09 10:04:45 -0300

    10/9/21

---
+++ Entities/AulaContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class AulaContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13;Database=aulasDb;Trusted_Connection=True;");
+            Console.WriteLine("tentou conectar");
+        }
+    
+        public DbSet<Aula> Aula { get; set; }
+    
+    }
+}
---
+++ Repository/AulaRepository.cs
@@ -0,0 +1,49 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class AulaRepository
+    {
+        private AulaContext _aulaContext;
+
+        public AulaRepository()
+        {
+            _aulaContext = new AulaContext();
+        }
+
+        public List<Aula> GetAulas()
+        {
+            return _aulaContext.Aula.ToList();
+        }
+
+        public Aula GetAulaById(string id)
+        {
+            return _aulaContext.Aula.FirstOrDefault(a => a.Id == id);
+        }
+
+        public Aula GetAulaBySubject (string subject)
+        {
+            return _aulaContext.Aula.FirstOrDefault(s => s.Subject == subject);
+        }
+
+        public async Task<Aula> AddAula(Aula aula)
+        {
+            try
+            {
+                var result = await _aulaContext.AddAsync(aula);
+                await _aulaContext.SaveChangesAsync();
+
+                return result.Entity;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error while saving changes: " + e);
+                return null;
+            }
+        }
+    }
+}
