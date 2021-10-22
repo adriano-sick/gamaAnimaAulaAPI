9e6fbe5e3d6a5905dd2c943dbe744e97ab83fc19
adriano-sick <sick.zero.boy@gmail.com>
2021-10-22 19:21:59 -0300

    test auto PR on github

--- gamaAnimaAulasAPI/Controllers/AulasController.cs
+++ gamaAnimaAulasAPI/Controllers/AulasController.cs
@@ -80,7 +80,7 @@ namespace gamaAnimaAulasAPI.Controllers
             }
         }
 
-        // DELETE: api/Aulas/5
+        // DELETE: api/Aulas/5 TEST CD PR ON GITHUB
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteAula(int id)
         {
