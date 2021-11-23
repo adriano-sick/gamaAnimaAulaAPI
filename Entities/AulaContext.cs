3d258a269ece10719654fce4bfbb9dead388e73a
Adriano Siqueira <63327884+adriano-sick@users.noreply.github.com>
2021-11-23 14:25:38 -0300

    Update AulaContext.cs

--- Entities/AulaContext.cs
+++ Entities/AulaContext.cs
@@ -17,6 +17,12 @@ namespace Entities
         //{
         //    optionsBuilder.UseSqlServer(@"Server=aulatest.mssql.somee.com; Database=aulatest; User Id=sickadri_SQLLogin_1; Password=8d97g9gkmt;");
         //}
+        
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //nao esta subindo as modificacoes daqui no git `-` testar - conteudo original, no txt aulas no desktop
+            optionsBuilder.UseSqlServer(Environment.GetEnvironmentVariable("AULACONNECTION"));
+        }
     
         public DbSet<Aula> Aula { get; set; }
     
